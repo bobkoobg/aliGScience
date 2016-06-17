f36789be53483e0d8fc5885a4ec28da78ee84a6c
bobkoobg <boyko.surlev@gmail.com>
2016-06-17 15:57:18 +0200

    Initial commit

---
+++ src/logic/AliGCalculator.java
@@ -0,0 +1,15 @@
+
+package logic;
+
+public class AliGCalculator {
+
+    public static void main( String[] args ) {
+        helloWorld();
+    }
+    
+    private static void helloWorld() {
+        System.out.println( "Techmology, Wha is it all about?"
+                + "\nis it GOOD or is it WAK?" );
+    }
+    
+}
